6641ebdf5606fd402fb7ab23e5c8fdee23a128ee
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-10-30 13:34:42 +0300

    refactor: use Action enum for operations

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,27 +1,26 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
-
-import { simpleCalculator } from '01-simple-tests';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    expect(simpleCalculator({ a: 2, b: 1, action: '+' })).toBe(3);
+    expect(simpleCalculator({ a: 2, b: 1, action: Action.Add })).toBe(3);
   });
 
   test('should subtract two numbers', () => {
-    expect(simpleCalculator({ a: 5, b: 3, action: '-' })).toBe(2);
+    expect(simpleCalculator({ a: 5, b: 3, action: Action.Subtract })).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    expect(simpleCalculator({ a: 5, b: 3, action: '*' })).toBe(15);
+    expect(simpleCalculator({ a: 5, b: 3, action: Action.Multiply })).toBe(15);
   });
 
   test('should divide two numbers', () => {
-    expect(simpleCalculator({ a: 10, b: 2, action: '/' })).toBe(5);
+    expect(simpleCalculator({ a: 10, b: 2, action: Action.Divide })).toBe(5);
   });
 
   test('should exponentiate two numbers', () => {
-    expect(simpleCalculator({ a: 10, b: 2, action: '^' })).toBe(100);
+    expect(simpleCalculator({ a: 10, b: 2, action: Action.Exponentiate })).toBe(
+      100,
+    );
   });
 
   test('should return null for invalid action', () => {
@@ -29,7 +28,9 @@ describe('simpleCalculator tests', () => {
   });
 
   test('should return null for invalid arguments', () => {
-    expect(simpleCalculator({ a: '10', b: 2, action: '/' })).toBeNull();
-    expect(simpleCalculator({ a: 10, b: '2', action: '/' })).toBeNull();
+    expect(
+      simpleCalculator({ a: '10', b: 2, action: Action.Divide }),
+    ).toBeNull();
+    expect(simpleCalculator({ a: 10, b: '2', action: Action.Add })).toBeNull();
   });
 });
