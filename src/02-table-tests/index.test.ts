e16d64b98013b0e0b7002a3011bebe6cd64691b7
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-10-30 15:55:56 +0300

    feat: add table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,31 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
-const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+const calculationTestCases = [
+  { a: 2, b: 1, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: -2, action: Action.Add, expected: 1 },
+  { a: 5, b: 3, action: Action.Subtract, expected: 2 },
+  { a: 5, b: 3, action: Action.Multiply, expected: 15 },
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 10, b: 2, action: Action.Exponentiate, expected: 100 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(calculationTestCases)(
+    'should calculate $a $action $b correctly',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
+
+  test.each`
+    description                               | action           | a       | b
+    ${'invalid operation'}                    | ${'%'}           | ${10}   | ${2}
+    ${'invalid first argument'}               | ${Action.Divide} | ${'10'} | ${2}
+    ${'invalid second argument'}              | ${Action.Add}    | ${10}   | ${'2'}
+    ${'both invalid arguments'}               | ${Action.Add}    | ${'10'} | ${'2'}
+    ${'both invalid arguments and operation'} | ${'%'}           | ${'10'} | ${'2'}
+  `(`should return null for $description`, ({ a, b, action }) => {
+    expect(simpleCalculator({ a, b, action })).toBeNull();
   });
-  // Consider to use Jest table tests API to test all cases above
 });
